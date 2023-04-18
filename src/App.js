6c570c1342e7e21cb1c4f856e74903efd1ba72a4
ngugiii <108797418+ngugiii@users.noreply.github.com>
2023-04-18 10:14:59 +0300

    added a scroll to top button and updated the responsive design

--- src/App.js
+++ src/App.js
@@ -7,14 +7,35 @@ import Projects from "./components/projects/Projects"
 import Footer from "./components/footer/Footer"
 import useLocalStorage from "react-use-localstorage";
 import Contact from "./components/contact/Contact";
+import { useEffect, useState } from "react";
+import { BsFillArrowUpSquareFill } from "react-icons/bs";
+
 
 function App() {
   const [theme, setTheme] = useLocalStorage("theme","light");
+  const [showButton, setShowButton] = useState(false);
 
   const toggleTheme =()=>{
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
   };
+
+  useEffect(()=>{
+    const handleScrollButtonVisibility =()=>{
+      window.pageYOffset > 600 ? setShowButton(true) : setShowButton(false);
+    };
+
+    window.addEventListener("scroll", handleScrollButtonVisibility);
+
+    return () => {
+      window.removeEventListener("scroll",handleScrollButtonVisibility);
+    };
+  }, []);
+
+  const handleScrollToTop =()=>{
+    window.scrollTo({ top: 0, behavior: "smooth"});
+  };
+
   return (
     <div className="main">
       <div className="header" data-theme={theme}>
@@ -25,8 +46,14 @@ function App() {
       <Education myTheme={theme}/>
       <Projects myTheme={theme}/>
       <Contact myTheme={theme}/>
+   {showButton &&
+   <div className="top" onClick={handleScrollToTop}>
+    <BsFillArrowUpSquareFill size={30} color="var(--color-secondary)"/>
+
+   </div> 
+   }
       <Footer myTheme={theme}/>
-      
+   {/* <button class="top" onClick={handleScrollToTop}>&uArr;</button> */}
     </div>
   );
 }
