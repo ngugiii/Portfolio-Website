38b385115b37e653243f3b6027c57bdf7b737435
ngugiii <erickngugi54@gmail.com>
2023-09-08 12:12:28 +0300

    changes

--- src/components/techStack/TechStack.js
+++ src/components/techStack/TechStack.js
@@ -1,16 +1,8 @@
 import React,{useEffect} from 'react'
-import html from "../../assets/html.png"
-import css from "../../assets/css.png"
-import javascript from "../../assets/javascript.png"
-import react from "../../assets/react.png"
-import git from "../../assets/git.png"
-import figma from "../../assets/figma.png"
-import firebase from "../../assets/firebase.png"
-import redux from "../../assets/redux.png"
 import "./TechStack.css"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-
+import { stackData } from './stack';
 
 const TechStack = (props) => {
   useEffect(()=>{
@@ -18,48 +10,24 @@ const TechStack = (props) => {
       duration: 1000,
     });
   },[])
+  console.log(stackData);
+
+  const data = stackData.map((stack)=>{
+    return (
+      <div className="stack">
+        <img src={stack.imgSRC} alt="" />
+        <span>{stack.name}</span>
+        </div>
+    )
+  })
   return (
     <section className="container tech-stack" id="tech-stack" data-theme={props.myTheme}>
       <h3 className='myHeadings'>Tech Stack</h3>
       <div className="stack-container">
       <div className="stacks" data-aos="flip-right">
-        <div className="stack">
-        <img src={html} alt="" />
-        <span>HTML</span>
-        </div>
-        <div className="stack">
-        <img src={css} alt="" />
-        <span>CSS</span>
-        </div>
-        <div className="stack">
-        <img src={javascript} alt="" />
-        <span>JavaScript</span>
-        </div>
-        <div className="stack">
-        <img src={react} alt="" />
-        <span>React</span>
-        </div>
-        <div className="stack">
-        <img src={redux} alt="" />
-        <span>Redux</span>
-        </div>
-        <div className="stack">
-        <img src={firebase} alt="" />
-        <span>Firebase</span>
-        </div>
-        <div className="stack">
-        <img src={git} alt="" />
-        <span>Git</span>
-        </div>
-        <div className="stack">
-        <img src={figma} alt="" />
-        <span>Figma</span>
-        </div>
+        {data}
       </div>
-
       </div>
-
-
     </section>
   )
 }
---
+++ src/components/techStack/stack.js
@@ -0,0 +1,58 @@
+import html from "../../assets/html.png"
+import css from "../../assets/css.png"
+import javascript from "../../assets/javascript.png"
+import react from "../../assets/react.png"
+import git from "../../assets/git.png"
+import figma from "../../assets/figma.png"
+import firebase from "../../assets/firebase.png"
+import redux from "../../assets/redux.png"
+import mongo from "../../assets/mongo.png"
+import node from "../../assets/node.png"
+import tailwind from "../../assets/tailwind.png"
+
+export const stackData = [
+    {
+        name:"HTML",
+        imgSRC:html,
+    },
+    {
+        name:"CSS",
+        imgSRC:css,
+    },
+    {
+        name:"JavaScript",
+        imgSRC:javascript,
+    },
+    {
+        name:"Tailwind CSS",
+        imgSRC:tailwind,
+    },
+    {
+        name:"Figma",
+        imgSRC:figma,
+    },
+    {
+        name:"React JS",
+        imgSRC:react,
+    },
+    {
+        name:"Redux",
+        imgSRC:redux,
+    },
+    {
+        name:"Firebase",
+        imgSRC:firebase,
+    },
+    {
+        name:"Node JS",
+        imgSRC:node,
+    },
+    {
+        name:"Mongo DB",
+        imgSRC:mongo,
+    },
+    {
+        name:"GIT",
+        imgSRC:git,
+    },
+]
