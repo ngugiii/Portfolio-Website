876115b3ecea7219c035cd663e90e160453c2daa
ngugiii <erickngugi54@gmail.com>
2023-09-25 14:51:40 +0300

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsInclude: /\.(png|jpg|jpeg|gif|svg)$/, // Include image files as assets
-  },
 })
